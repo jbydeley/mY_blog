e202ae914dff89ca0be60777f66ad1a95916d30f
Jared Bydeley <jared@roadmunk.com>
2021-07-09 10:21:10 -0400

    Initial Commit

---
+++ lib/my_blog_web/endpoint.ex
@@ -0,0 +1,89 @@
+defmodule MyBlogWeb.Endpoint do
+  use Phoenix.Endpoint, otp_app: :my_blog
+  use SiteEncrypt.Phoenix
+
+  @impl Phoenix.Endpoint
+  def init(_key, config) do
+    {:ok, SiteEncrypt.Phoenix.configure_https(config)}
+  end
+
+  @impl SiteEncrypt
+  def certification do
+    SiteEncrypt.configure(
+      client: :native,
+      domains: ["bydeley.dev", "www.bydeley.dev"],
+      emails: ["jared@bydeley.ca"],
+      db_folder: Application.get_env(:my_blog, :cert_path, "tmp/site_encrypt_db"),
+      directory_url:
+        case Application.get_env(:my_blog, :cert_mode, "local") do
+          "local" -> {:internal, port: 4002}
+          "staging" -> "https://acme-staging-v02.api.letsencrypt.org/directory"
+          "production" -> "https://acme-v02.api.letsencrypt.org/directory"
+        end
+    )
+  end
+
+  # The session will be stored in the cookie and signed,
+  # this means its contents can be read but not tampered with.
+  # Set :encryption_salt if you would also like to encrypt it.
+  @session_options [
+    store: :cookie,
+    key: "_my_blog_key",
+    signing_salt: "awg0RfT2"
+  ]
+
+  socket "/socket", MyBlogWeb.UserSocket,
+    websocket: true,
+    longpoll: false
+
+  socket "/live", Phoenix.LiveView.Socket, websocket: [connect_info: [session: @session_options]]
+
+  def www_redirect(conn, _options) do
+    if String.starts_with?(conn.host, "www.#{host()}") do
+      conn
+      |> Phoenix.Controller.redirect(external: "https://#{host()}")
+      |> halt()
+    else
+      conn
+    end
+  end
+
+  # Redirect all www requests to the root url
+  plug :www_redirect
+
+  # Serve at "/" the static files from "priv/static" directory.
+  #
+  # You should set gzip to true if you are running phx.digest
+  # when deploying your static files in production.
+  plug Plug.Static,
+    at: "/",
+    from: :my_blog,
+    gzip: false,
+    only: ~w(css fonts images js favicon.ico robots.txt)
+
+  # Code reloading can be explicitly enabled under the
+  # :code_reloader configuration of your endpoint.
+  if code_reloading? do
+    socket "/phoenix/live_reload/socket", Phoenix.LiveReloader.Socket
+    plug Phoenix.LiveReloader
+    plug Phoenix.CodeReloader
+    plug Phoenix.Ecto.CheckRepoStatus, otp_app: :my_blog
+  end
+
+  plug Phoenix.LiveDashboard.RequestLogger,
+    param_key: "request_logger",
+    cookie_key: "request_logger"
+
+  plug Plug.RequestId
+  plug Plug.Telemetry, event_prefix: [:phoenix, :endpoint]
+
+  plug Plug.Parsers,
+    parsers: [:urlencoded, :multipart, :json],
+    pass: ["*/*"],
+    json_decoder: Phoenix.json_library()
+
+  plug Plug.MethodOverride
+  plug Plug.Head
+  plug Plug.Session, @session_options
+  plug MyBlogWeb.Router
+end
